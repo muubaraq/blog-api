3da2212a8c984d679368008c626a1ae5acecd7da
mubaraq <paynedaze@gmail.com>
2022-06-15 19:55:18 +0100

    ch: view, create , delete and get post

---
+++ js/app.js
@@ -0,0 +1,77 @@
+
+
+
+//
+let postsArray = []
+const titleInput = document.getElementById("post-title")
+const bodyInput = document.getElementById("post-body")
+const form = document.getElementById("new-post")
+
+
+// 
+
+function renderPosts() {
+    let html = ""
+    for (let post of postsArray) {
+        html += `
+        <div class="col-md-12">
+        <div class="card mb-3 mt-2">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="img/blog-1.jpg" class="img-fluid rounded-start h-100 w-100" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${post.title}</h5>
+                <p class="card-text">${post.body}</p>
+                <button class="btn btn-primary">Update</button>
+                <a href="detail.html?id=${post.id}" class="btn btn-secondary">View</a>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+        `
+    }
+    document.getElementById("post-container").innerHTML = html
+}
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => {
+        postsArray = data.slice(0, 15)
+        renderPosts()
+    })
+
+    // js to add a post
+
+document.getElementById("new-post").addEventListener("submit", function(e) {
+    e.preventDefault()
+    const postTitle = titleInput.value
+    const postBody = bodyInput.value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+    
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    
+    fetch("https://jsonplaceholder.typicode.com/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()
+
+            // clear the form
+            // titleInput.value = ""
+            // bodyInput.value = ""
+            form.reset()
+        })
+})
---
+++ js/detail.js
@@ -0,0 +1,25 @@
+// js for detail.html
+const id = new URLSearchParams(window.location.search).get("id");
+const container = document.querySelector(".details");
+const deleteBtn = document.querySelector("#delete");
+
+const renderDetails = async () => { 
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const post = await res.json();
+    
+  const  template = `
+    <img src="img/blog-1.jpg" class="img-fluid rounded-start" alt="...">
+    <h1>${post.title}</h1>
+    <p>${post.body}</p>
+    `
+    container.innerHTML = template;
+}
+
+deleteBtn.addEventListener("click", async (e) => {
+   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: "DELETE"
+})
+    window.location.replace("index.html");
+})
+
+window.addEventListener('DOMContentLoaded', () => renderDetails());
